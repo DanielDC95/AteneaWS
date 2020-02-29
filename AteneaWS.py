aaa3f913405a2ab11f86cac02c4ed44f41302e27
DanielDC95 <ddc95@live.com>
2020-02-28 23:24:46 -0500

    Se crea la conexion a la base de datos de postgres y se agrega la creacion y consulta de datos en la tabla genre"

--- AteneaWS.py
+++ AteneaWS.py
@@ -1,11 +1,46 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from flask import request
+import psycopg2
+import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+conn = psycopg2.connect(
+    database="",
+    user="",
+    password="",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+@app.route("/get_genres", methods=['GET'])
+def get_genres():
     if (request.method == 'GET'):
-        return jsonify({"about":"Hello World!"})
+        query_result = []
+        cursor_dic = {}
+        cursor.execute("Select * From genres;")
+        #row = cursor.__next__()
+        for row in cursor:
+            cursor_dic = {}
+
+            for col, description in enumerate(cursor.description):
+                cursor_dic.update({description[0] : row[col]})
+            
+            query_result.append(cursor_dic)
+
+        return jsonify(query_result)
+
+
+@app.route("/add_genre", methods = ['POST'])
+def add_genre():
+    if request.method == 'POST':
+        request_json = request.get_json()
+        
+        cursor.execute("Insert Into genres (name) Values ('{}');".format(request_json["name"]))
+        conn.commit()
+        return jsonify({"about":"Success"})
 
 # @app.route('/multi/<int:num>', methods=['GET'])
 # def get_multiply10(num):
